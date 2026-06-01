a2a7885cf39cb0d4e89380ac36ae48f25da2800c
JustinNQ <1546021@senati,pe>
2026-06-01 15:58:08 -0500

    Conexion Supabase y login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function iniciarSesion(e: React.FormEvent) {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+  alert(error.message);
+  console.log(error);
+  return;
+}
+
+    window.location.href = "/dashboard";
+  }
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-3xl shadow-xl w-full max-w-md">
@@ -6,16 +31,20 @@ export default function Login() {
           Acceso Alumnos
         </h1>
 
-        <form className="space-y-4">
+        <form onSubmit={iniciarSesion} className="space-y-4">
           <input
             type="email"
             placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full border rounded-xl p-3 text-gray-900"
           />
 
           <input
             type="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full border rounded-xl p-3 text-gray-900"
           />
 
